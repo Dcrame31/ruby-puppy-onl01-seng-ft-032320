376dc46bcdf20bcd07b9aa7d53236ac54050d07f
Diane Crame <dcrame@icloud.com>
2020-03-31 14:47:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,15 +3,12 @@
 class Dog
  
   @@all = []
-  @@name = []
-
+  
   attr_accessor :name
  
   def initialize(name)
     @name = name
     @@all << self
-    @@name << name
-    
   end
  
   def self.all
